583596f5f98f57f8bcba85f875fc3864a5605f2a
reka-tama <rekatama0608@gmail.com>
2024-12-31 15:59:17 +0700

    change : merubah indentasi

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+	return view('home');
 });
 
 Route::get('/about', function () {
-    return view('about');
+	return view('about');
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+	return view('contact');
 });
 
 Route::get('/blog', function () {
-    return view('blog');
+	return view('blog');
 });
